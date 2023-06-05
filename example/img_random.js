00d8c094f7843dd15dfcfc59707e54cd5d13a06c
$artnp <99397593+artnp@users.noreply.github.com>
2023-06-05 13:49:45 +0700

    Add files via upload

---
+++ example/img_random.js
@@ -0,0 +1,10 @@
+function randomR(){
+    /////////แก้จำนวน////////////////////////////
+    randomNumber = Math.floor(Math.random() * 11) + 1;
+    ////////////////////////////////////////////
+
+    imageName = randomNumber + ".jpg";
+    document.getElementById('original').innerHTML='<img src="img/' + imageName + '" >'
+    document.getElementById('retouch').innerHTML='<img src="img/' + randomNumber + 'b.jpg' + '">'
+  }
+  randomR()
