5ec125ec801bbcc967320ec4bed41745ae1122ba
$artnp <99397593+artnp@users.noreply.github.com>
2023-06-19 22:34:45 +0700

    Update script.js

--- example/addon/script.js
+++ example/addon/script.js
@@ -29,7 +29,6 @@ function sliderMouseMove(event) {
     mouseX = 20; // Set mouseX to 0 to move the mouse to the left
     sliderImgWrapper.style.width = '100%';
     sliderHandle.style.left = `calc(${((mouseX / sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth / 2}px)`;
-	randomR2()
     randomR()
     changePattern()
     
