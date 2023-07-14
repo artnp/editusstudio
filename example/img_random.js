deabc9a885a17675a5a34625c3e7de8d6d0821ca
$artnp <99397593+artnp@users.noreply.github.com>
2023-07-14 12:04:05 +0700

    Update img_random.js

--- example/img_random.js
+++ example/img_random.js
@@ -2,8 +2,8 @@ function randomR() {
     /////////แก้จำนวน////////////////////////////
     randomNumber = Math.floor(Math.random() * 52) + 1;
     ////////////////////////////////////////////
-    imageName = randomNumber + ".jpg";
+    imageName = randomNumber + " (1).jpg";
     document.getElementById('original').innerHTML='<img src="img/' + imageName + '" >'
-    document.getElementById('retouch').innerHTML='<img src="img/' + randomNumber + 'b.jpg' + '">'
+    document.getElementById('retouch').innerHTML='<img src="img/' + randomNumber + ' (2).jpg' + '">'
 }
         randomR()
