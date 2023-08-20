841758f584344c41330e6f9e0c996c3401ed3882
$artnp <99397593+artnp@users.noreply.github.com>
2023-08-21 00:02:53 +0700

    Update script.js

--- example/addon/script.js
+++ example/addon/script.js
@@ -33,32 +33,16 @@ function sliderMouseMove(event) {
 
 
 
-const end = Date.now() + 15 * 1000;
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-// go Buckeyes!
-const colors = ["#bb0000", "#ffffff"];
-
-(function frame() {
-  confetti({
-    particleCount: 2,
-    angle: 60,
-    spread: 55,
-    origin: { x: 0 },
-    colors: colors,
-  });
-
-  confetti({
-    particleCount: 2,
-    angle: 120,
-    spread: 55,
-    origin: { x: 1 },
-    colors: colors,
-  });
-
-  if (Date.now() < end) {
-    requestAnimationFrame(frame);
-  }
-})();
+confetti({
+  angle: randomInRange(55, 125),
+  spread: randomInRange(50, 70),
+  particleCount: randomInRange(50, 100),
+  origin: { y: 0.6 },
+});
     
 
     randomR()
