ff864de293bf39016f1712cedd0ff88acf734bfa
$artnp <99397593+artnp@users.noreply.github.com>
2023-08-20 22:48:01 +0700

    Update script.js

--- example/addon/script.js
+++ example/addon/script.js
@@ -25,13 +25,10 @@ function sliderMouseMove(event) {
   sliderImgWrapper.style.width = `${((1 - mouseX / sliderWidth) * 100).toFixed(4)}%`;
   sliderHandle.style.left = `calc(${((mouseX / sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth / 2}px)`;
 
-
+///////////////////////////////////////////////////////////////////////////////////////
   if (mouseX === sliderWidth) {
 
-    party.confetti(runButton, {
-    count: party.variation.range(20, 40)
-    });
-    
+
     mouseX = 20; // Set mouseX to 0 to move the mouse to the left
     sliderImgWrapper.style.width = '100%';
     sliderHandle.style.left = `calc(${((mouseX / sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth / 2}px)`;
