42007108a3a01e0657e90e1d22d97ca4caadc2f0
$artnp <99397593+artnp@users.noreply.github.com>
2023-08-24 17:21:30 +0700

    Update script_new.js

--- example/addon/script_new.js
+++ example/addon/script_new.js
@@ -27,7 +27,7 @@ function sliderMouseMove(event) {
   
 ///////////////////////////////////////////////////////////////////////////////////////
   if (mouseX === sliderWidth) {
-    mouseX = 20; // Set mouseX to 0 to move the mouse to the left
+    mouseX = 50; // Set mouseX to 0 to move the mouse to the left
     sliderImgWrapper.style.width = '100%';
     sliderHandle.style.left = `calc(${((mouseX / sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth / 2}px)`;
 
