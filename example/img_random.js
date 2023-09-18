3e3197d09619ca8a58dbab9c019cb0e6486627c8
$artnp <99397593+artnp@users.noreply.github.com>
2023-09-19 00:30:32 +0700

    Update img_random.js

--- example/img_random.js
+++ example/img_random.js
@@ -1,18 +1,24 @@
-function randomR() {
-    // สุ่มเลขจำนวนเต็มระหว่าง 1 ถึง 200
-    const randomNumber = Math.floor(Math.random() * 200) + 1;
-    
-    // กำหนดชื่อไฟล์รูปภาพ
+function loadRandomImage() {
+    const maxImageNumber = 200;
+    const randomNumber = Math.floor(Math.random() * maxImageNumber) + 1;
     const imageName = `${randomNumber} (1).jpg`;
-    
-    // กำหนด URL ของรูปภาพและแสดงใน HTML
+    const imageUrl = `https://artnp.github.io/editusstudio/example/img/${imageName}`;
+
     const originalImageElement = document.getElementById('original');
-    originalImageElement.innerHTML = `<img src="https://artnp.github.io/editusstudio/example/img/${imageName}" >`;
-    
-    // กำหนด URL ของรูปภาพที่แก้ไขแล้วและแสดงใน HTML
     const retouchImageElement = document.getElementById('retouch');
+
+    // ตรวจสอบว่ารูปภาพโหลดไม่สำเร็จ (img not found)
+    const img = new Image();
+    img.src = imageUrl;
+    img.onerror = function () {
+        // เรียกฟังก์ชันอีกรอบถ้ารูปภาพไม่พบ
+        loadRandomImage();
+    };
+
+    // แสดงรูปภาพใน HTML
+    originalImageElement.innerHTML = `<img src="${imageUrl}" >`;
     retouchImageElement.innerHTML = `<img src="https://artnp.github.io/editusstudio/example/img/${randomNumber} (2).jpg">`;
 }
 
-// เรียกใช้ฟังก์ชัน randomR
-randomR();
+// เรียกใช้ฟังก์ชันเพื่อโหลดรูปภาพแรก
+loadRandomImage();
