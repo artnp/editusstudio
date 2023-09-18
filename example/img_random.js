5a2910e96194953b6451805f77ca1809f9699ff2
$artnp <99397593+artnp@users.noreply.github.com>
2023-09-19 00:24:43 +0700

    Update img_random.js

--- example/img_random.js
+++ example/img_random.js
@@ -1,17 +1,18 @@
-function replaceImageOnError(imageElement, baseImageUrl) {
-    imageElement.onerror = function () {
-        const randomNumber = Math.floor(Math.random() * 200) + 1;
-        const newImageUrl = baseImageUrl + randomNumber + " (1).jpg";
-        imageElement.src = newImageUrl;
-    };
+function randomR() {
+    // สุ่มเลขจำนวนเต็มระหว่าง 1 ถึง 200
+    const randomNumber = Math.floor(Math.random() * 200) + 1;
+    
+    // กำหนดชื่อไฟล์รูปภาพ
+    const imageName = `${randomNumber} (1).jpg`;
+    
+    // กำหนด URL ของรูปภาพและแสดงใน HTML
+    const originalImageElement = document.getElementById('original');
+    originalImageElement.innerHTML = `<img src="https://artnp.github.io/editusstudio/example/img/${imageName}" >`;
+    
+    // กำหนด URL ของรูปภาพที่แก้ไขแล้วและแสดงใน HTML
+    const retouchImageElement = document.getElementById('retouch');
+    retouchImageElement.innerHTML = `<img src="https://artnp.github.io/editusstudio/example/img/${randomNumber} (2).jpg">`;
 }
 
-// ใช้ฟังก์ชันนี้ในการกำหนดการกระทำเมื่อรูปภาพไม่พบสำหรับแต่ละรูป
-const originalImage = document.getElementById('original').querySelector('img');
-const retouchImage = document.getElementById('retouch').querySelector('img');
-
-const originalBaseUrl = "https://artnp.github.io/editusstudio/example/img/";
-const retouchBaseUrl = "https://artnp.github.io/editusstudio/example/img/";
-
-replaceImageOnError(originalImage, originalBaseUrl);
-replaceImageOnError(retouchImage, retouchBaseUrl);
+// เรียกใช้ฟังก์ชัน randomR
+randomR();
