573a955ba3616500871075bf87226fd457145dfa
$artnp <99397593+artnp@users.noreply.github.com>
2024-03-26 21:01:21 +0700

    Update img_random.js

--- example/img_random.js
+++ example/img_random.js
@@ -1,6 +1,6 @@
 function randomR() {
     /////////แก้จำนวน////////////////////////////
-    randomNumber = Math.floor(Math.random() * 192) + 1;
+    randomNumber = Math.floor(Math.random() * 198) + 1;
     ////////////////////////////////////////////
     imageName = randomNumber + " (1).jpg";
     document.getElementById('original').innerHTML='<img src="https://artnp.github.io/editusstudio/example/img/' + imageName + '" >'
