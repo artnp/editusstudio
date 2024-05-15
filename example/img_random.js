a0fb0231b4d013b85be8f30ed62cc5c6c08b09f0
$artnp <99397593+artnp@users.noreply.github.com>
2024-05-15 22:14:44 +0700

    Add files via upload

--- example/img_random.js
+++ example/img_random.js
@@ -1,9 +1,13 @@
 function randomR() {
     /////////แก้จำนวน////////////////////////////
-    randomNumber = Math.floor(Math.random() * 198) + 1;
-    ////////////////////////////////////////////
+    if (localStorage.getItem('lastestUpdate') !== null) {
+        randomNumber = Math.floor(Math.random() * Number(localStorage.getItem('lastestUpdate'))) + 1;
+      } else {
+        randomNumber = Math.floor(Math.random() * 200) + 1;
+      }
+    
     imageName = randomNumber + " (1).jpg";
-    document.getElementById('original').innerHTML='<img src="https://artnp.github.io/editusstudio/example/img/' + imageName + '" >'
-    document.getElementById('retouch').innerHTML='<img src="https://artnp.github.io/editusstudio/example/img/' + randomNumber + ' (2).jpg' + '">'
+    document.getElementById('original').innerHTML='<img src="img/' + imageName + '" >'
+    document.getElementById('retouch').innerHTML='<img src="img/' + randomNumber + ' (2).jpg' + '">'
 }
         randomR()
