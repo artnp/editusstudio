3238879ef801c23bcc294fedc06f2513d87bdd55
$artnp <99397593+artnp@users.noreply.github.com>
2024-05-19 21:38:27 +0700

    Update img_random.js

--- example/img_random.js
+++ example/img_random.js
@@ -7,7 +7,7 @@ function randomR() {
       }
     
     imageName = randomNumber + " (1).jpg";
-    document.getElementById('original').innerHTML='<img src="../example/img/' + imageName + '" >'
-    document.getElementById('retouch').innerHTML='<img src="../example/img/' + randomNumber + ' (2).jpg' + '">'
+    document.getElementById('original').innerHTML='<img src="https://artnp.github.io/editusstudio/example/img/' + imageName + '" >'
+    document.getElementById('retouch').innerHTML='<img src="https://artnp.github.io/editusstudio/example/img/' + randomNumber + ' (2).jpg' + '">'
 }
         randomR()
