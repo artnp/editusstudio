e8fc16accb8083abc9b021908cac3c34400e5d0c
$artnp <99397593+artnp@users.noreply.github.com>
2024-05-19 21:30:53 +0700

    Add files via upload

--- example/img_random.js
+++ example/img_random.js
@@ -7,7 +7,7 @@ function randomR() {
       }
     
     imageName = randomNumber + " (1).jpg";
-    document.getElementById('original').innerHTML='<img src="img/' + imageName + '" >'
-    document.getElementById('retouch').innerHTML='<img src="img/' + randomNumber + ' (2).jpg' + '">'
+    document.getElementById('original').innerHTML='<img src="../example/img/' + imageName + '" >'
+    document.getElementById('retouch').innerHTML='<img src="../example/img/' + randomNumber + ' (2).jpg' + '">'
 }
         randomR()
